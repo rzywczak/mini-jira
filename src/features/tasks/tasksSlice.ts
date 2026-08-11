fd2566beacfff2e03d1f1e15618f5a2a604925cb
Rafal <rzywczak99@gmail.com>
2026-08-11 11:25:24 +0200

    refactor: remove commented-out initial task data from TaskState

--- src/features/tasks/tasksSlice.ts
+++ src/features/tasks/tasksSlice.ts
@@ -15,29 +15,6 @@ interface TaskState {
     tasks: Task[];
 }
 
-// const initialState: TaskState = {
-//     tasks: [
-//         {
-//             id: '1',
-//             title: 'task-1',
-//             description: 'task 1 description',
-//             status: 'todo',
-//         },
-//         {
-//             id: '2',
-//             title: 'task-2',
-//             description: 'task 2 description',
-//             status: 'inProgress',
-//         },
-//         {
-//             id: '3',
-//             title: 'task-3',
-//             description: 'task 3 description',
-//             status: 'done',
-//         },
-//     ],
-// };
-
 const initialState: TaskState = {
     tasks: loadTasks(),
 };
